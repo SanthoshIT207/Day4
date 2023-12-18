471f15cf6cc746572ae87f2c4bf9b1bd205f9947
SanthoshIT207 <153698713+SanthoshIT207@users.noreply.github.com>
2023-12-18 12:15:59 +0530

    Add files via upload

---
+++ ClassComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class ClassComponent extends Component{
+    render()
+    {
+        return (
+            <div>
+                <h1>Velu</h1>
+            </div>
+        )
+    }
+}
---
+++ FunctionalCom.js
@@ -0,0 +1,9 @@
+function FunctionalCom()
+{
+    return (
+        <div>
+             <h1>Gilli</h1>
+        </div>
+    )
+}
+export default FunctionalCom;
